07a616eb7cf0c89669c6ecfc70fe11eead89fe29
James Brunner <james.brunner@kaelus.com>
2016-01-07 09:37:13 -0700

    Convert 'sensor boards' to channels and move towards list/array usage

--- hwloop.py
+++ hwloop.py
@@ -49,17 +49,36 @@ print("SPI bus 0: Enabled")
 avalanche.spiBus0isolate(False)
 
 
-#setup sensor boards
-sensor0 = stpm3x(spi0dev0)
-sensor1 = stpm3x(spi0dev1)
+# setup sensor boards (== 'channels')
+channels = [ stpm3x(spi0dev0), stpm3x(spi0dev1) ]
 
+print 'Configuring %d channels:' % len(channels)
+
+for i, channel in enumerate(channels):
+	cfg = config.system['sensors'][i]
+
+	print '\nChannel %d ...' % i
+
+	for g in ['GAIN1', 'GAIN2']:
+		if not g in cfg:
+			print '    %s configuration missing' % g
+
+	status = 0
+	status |= channel.write(STPM3X.GAIN1, cfg['GAIN1'])
+	status |= channel.write(STPM3X.GAIN2, cfg['GAIN2'])
+
+	if not status:
+		print '    error configuring channel %d' % i
+
+
+'''
 #sensor 1
 cfg = config.system['sensors'][0]
 #print(bin(cfg['GAIN1']))
 print("#Sensors",len(config.system['sensors']))
 
 if not 'GAIN1' in cfg:
-    print("\nNo GAIN1 Configuration found")
+	print("\nNo GAIN1 Configuration found")
 
 status = 0
 
@@ -67,7 +86,7 @@ status |= sensor0.write(STPM3X.GAIN1, cfg['GAIN1'])
 status |= sensor0.write(STPM3X.GAIN2, cfg['GAIN2'])
 
 if not status == 0:
-    print ("Error configuring sensor 0")
+	print ("Error configuring sensor 0")
 
 
 #sensor 2 configuration
@@ -78,42 +97,42 @@ status |= sensor1.write(STPM3X.GAIN1, cfg['GAIN1'])
 status |= sensor1.write(STPM3X.GAIN2, cfg['GAIN2'])
 
 if not status == 0:
-    print ("Error configuring sensor 1")
+	print ("Error configuring sensor 1")
+'''
 
 
-
-print("\nLoop Started...")
+print("\nLoop starting...")
 
 while(1):
-    timestamp = time.time()
-    #print("Sync Time: " + str(timestamp))
 
-    #synchronize sensors
-    avalanche.syncSensors()
+	# synchronize sensors - get timestamp for data points
+	timestamp = avalanche.syncSensors()
 
-    #read back sensor data
-    v0 = sensor0.read(STPM3X.V2RMS)  * 0.035430    
-    c0 = sensor0.gatedRead(STPM3X.C2RMS, 7) * 0.003333
-    v1 = sensor0.read(STPM3X.V1RMS) * 0.035430 
-    c1 = sensor0.gatedRead(STPM3X.C1RMS, 7) * 0.003333
-    #v2 = sensor1.read(STPM3X.V2RMS)
-    #c2 = sensor1.read(STPM3X.C2RMS)
-    #v3 = sensor1.read(STPM3X.V1RMS)
-    #c3 = sensor1.read(STPM3X.C1RMS)
+	'''		
+	v0 = sensor0.read(STPM3X.V2RMS)  * 0.035430    
+	c0 = sensor0.gatedRead(STPM3X.C2RMS, 7) * 0.003333
+	v1 = sensor0.read(STPM3X.V1RMS) * 0.035430 
+	c1 = sensor0.gatedRead(STPM3X.C1RMS, 7) * 0.003333
+	#v2 = sensor1.read(STPM3X.V2RMS)
+	#c2 = sensor1.read(STPM3X.C2RMS)
+	#v3 = sensor1.read(STPM3X.V1RMS)
+	#c3 = sensor1.read(STPM3X.C1RMS)
+	'''
 
-    cmedata.status['channels'][0]['sensors'][0]['data'][0] = [timestamp, v0]
-    cmedata.status['channels'][0]['sensors'][1]['data'][0] = [timestamp, c0]
-    cmedata.status['channels'][1]['sensors'][0]['data'][0] = [timestamp, v1]
-    cmedata.status['channels'][1]['sensors'][1]['data'][0] = [timestamp, c1]
+	# reach each channels' sensors and update cme status
+	for i, channel in enumerate(channels):
+		cmedata.status['channels'][i]['sensors'][0]['data'][0] = [ timestamp, channel.read(STPM3X.V2RMS) * 0.035430 ]
+		cmedata.status['channels'][i]['sensors'][1]['data'][0] = [ timestamp, channel.gatedRead(STPM3X.C2RMS) * 0.003333 ]
 
-    #update shared memory object
-    sharedmem.set('status', cmedata.status)
-    
-    #print(cmedata.status['channels'][0]['sensors'][0]['data'][0])
-    
-    #print("V1RMS: " + str(v1) + " | C1RMS: " + str(c1))
-    #print("V2RMS: " + str(v2) + " | C2RMS: " + str(c2))
-    #print("V3RMS: " + str(v3) + " | C3RMS: " + str(c3))
-    #print("V4RMS: " + str(v4) + " | C4RMS: " + str(c4))
+	#update shared memory object
+	sharedmem.set('status', cmedata.status)
+	
+	print '%f, %f' % (cmedata.status['channels'][0]['sensors'][0]['data'][0], 
+					  cmedata.status['channels'][0]['sensors'][1]['data'][0])
+	
+	#print("V1RMS: " + str(v1) + " | C1RMS: " + str(c1))
+	#print("V2RMS: " + str(v2) + " | C2RMS: " + str(c2))
+	#print("V3RMS: " + str(v3) + " | C3RMS: " + str(c3))
+	#print("V4RMS: " + str(v4) + " | C4RMS: " + str(c4))
 
-    time.sleep(0.5)
+	time.sleep(0.5)
