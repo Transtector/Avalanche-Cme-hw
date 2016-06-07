188d7e36e8dc84050dfc6049c05419770ba41241
James Brunner <james.brunner@smithsmicrowave.com>
2016-06-07 15:13:42 -0600

    Add some commentary to show how to connect to the cme-mc container from either cme-hw container (e.g., a "normal" installed setup) or from the machine hosting the cme-mc container.

--- cmehw/config.py
+++ cmehw/config.py
@@ -17,15 +17,29 @@ APPLOG = os.path.join(LOGDIR, 'cme-hw.log')
 LOGBYTES = 1024 * 10
 LOGCOUNT = 5
 
-# create the log directory if necessary
-if not os.path.exists(LOGDIR):
-	os.makedirs(LOGDIR)
 
-# Memcache server is generally the same machine (localhost or 127.0.0.1)
-# but if you comment out the appropriate line in /etc/memcached.conf,
-# then external machines (e.g., Cme running on another machine) can
-# connect and use the Cme-hw running here.
-MEMCACHE = 'cme-memcached:11211'
+# Memcached is typically running in a docker container and memcache 
+# clients will be running in their own docker containers (on the same
+# docker host).  The containers are able to "link" by referencing
+# the container alias given in the docker run command:
+#
+#	$ docker run --rm -ti --link cme-mc:cme-mc <my-image>
+#
+#	where "--link cme-mc:cme-mc" means link the container named 'cme-mc'
+#	to the container we're running using the host alias 'cme-mc'.
+#
+# If you're trying to run cmehw from the machine hosting the
+# cme-mc container, it works slightly differently, and you'll
+# have to either edit this configuration setting to something
+# like '127.0.0.1:11211' or you can add a line to your /etc/hosts
+# file that maps the local host to the alias name as in:
+#
+#	-- lines in /etc/hosts --
+# 	127.0.0.1	localhost
+#	127.0.0.1	cme-mc
+#	...
+#
+MEMCACHE = 'cme-mc:11211'
 
 
 # Channels keep arrays of their sensor and control data every time
@@ -56,17 +70,16 @@ MEMCACHE = 'cme-memcached:11211'
 
 LOG_MAX_SIZE = 200000 # gives log file sizes somewhere under 10 MB 
 
+
 # Sensor read/publish/log loop repeats this number of seconds.
 # Change this in conjunction with the LOG_MAX_SIZE above, as
 # every time through loop logs a record (line) for every (non-errored)
 # channel on the Cme.
 
-
-
-
 # Data log history length is approximately 
 # ( LOG_MAX_SIZE * LOOP_PERIOD_s ) / ( 3600 * 24) days
 LOOP_PERIOD_s = 1.0
+
 
 # Discharge sensors for this long before enabling SPI bus
 SENSOR_CAPS_DISCHARGE_WAIT_s = 10
