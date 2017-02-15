f7a79738a5af5df038260ba03020d27787952c14
James Brunner <james.brunner@smithsmicrowave.com>
2017-02-15 11:44:04 -0700

    Use the VERSION file for package version.

--- setup.py
+++ setup.py
@@ -1,11 +1,15 @@
+import os
 from setuptools import setup
+
+# version is stored in the VERSION file in package root
+with open(os.path.join(os.getcwd(), 'VERSION')) as f:
+	version = f.readline().strip()
 
 setup (
 	name				= "cmehw",
-	version				= "0.1",
-	description 		= "CME hardware interface",
+	version				= version,
+	description			= "CME hardware interface",
 	packages			= ['cmehw'],
 	install_requires	= ["crcmod", "rrdtool", "RPi.GPIO",	"spidev" ],
 	entry_points		= {'console_scripts': ['cmehw = cmehw.__main__:main']}
 )
-
