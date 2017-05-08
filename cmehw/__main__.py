b74058bcb8f027c7e8dec20c4ee12ff1d4e2bf0c
James Brunner <james.brunner@smithsmicrowave.com>
2017-05-08 15:53:18 -0600

    Use new common logging module

--- cmehw/__main__.py
+++ cmehw/__main__.py
@@ -2,17 +2,16 @@
 
 import sys, time, signal
 
-from .common import Config
+from .common import Config, Logging
 
-from .Logging import Logger
 from .Avalanche import Avalanche
 from .RRD import RRD
-
 from .Thresholds import ProcessAlarms
-
 from .Alarms import AlarmManager
 
 SHUTDOWN_FLAG = False
+LOGGER = None
+
 
 def cleanup(signum=None, frame=None):
 	global SHUTDOWN_FLAG
@@ -20,14 +19,7 @@ def cleanup(signum=None, frame=None):
 		return
 
 	SHUTDOWN_FLAG = True # stop main loop
-	Logger.info("Shutdown detected - cleaning up")
-
-
-# SIGTERM signal handler - called at shutdown (see common/Reboot.py)
-# This lets us reboot/halt from other code modules without having
-# GPIO included in them.
-signal.signal(signal.SIGTERM, cleanup)
-signal.signal(signal.SIGHUP, cleanup)
+	LOGGER.info("Shutdown detected - cleaning up")
 
 
 def main(args=None):
@@ -36,7 +28,30 @@ def main(args=None):
 	if args is None:
 		args = sys.argv[1:]
 
-	Logger.info("Avalanche (Cme-hw) is rumbling...")
+	# Log to console/screen too
+	CONSOLE_LOGGING = False
+	
+	global LOGGER
+	LOGGER = Logging.GetLogger('cmehw', {
+		'REMOVE_PREVIOUS': True,
+		'PATH': os.path.join(Config.PATHS.LOGDIR, 'cme-hw.log'),
+		'SIZE': (1024 * 10),
+		'COUNT': 1,
+		'FORMAT': '%(asctime)s %(levelname)-8s [%(name)s] %(message)s',
+		'DATE': '%Y-%m-%d %H:%M:%S',
+		'LEVEL': 'DEBUG',
+		'CONSOLE': CONSOLE_LOGGING
+	})
+
+	LOGGER.info("Avalanche (Cme-hw) is rumbling...")
+
+
+	# SIGTERM signal handler - called at shutdown (see common/Reboot.py)
+	# This lets us reboot/halt from other code modules without having
+	# GPIO included in them.
+	LOGGER.info("Listening for shutdown signals")
+	signal.signal(signal.SIGTERM, cleanup)
+	signal.signal(signal.SIGHUP, cleanup)
 
 	spinners = "|/-\\"
 	spinner_i = 0
@@ -69,7 +84,7 @@ def main(args=None):
 
 		# debug/print channel values
 		#cc = "\n".join([ "{0}".format(ch.debugPrint()) for ch in channels ])
-		#Logger.debug(cc)		
+		#LOGGER.debug(cc)		
 			
 		# "\x1b[K" is ANSII clear to end of line
 		#sys.stdout.write("\tHardware looping [{0:.3f} s, {1:.3f} s] {2}\x1b[K\r".format(process_time, delay_time, spinners[spinner_i])) 
@@ -84,10 +99,10 @@ if __name__ == "__main__":
 		main()
 
 	except KeyboardInterrupt:
-		Logger.info("Avalanche (Cme-hw) shutdown requested ... exiting")
+		LOGGER.info("Avalanche (Cme-hw) shutdown requested ... exiting")
 
 	except Exception as e:
-		Logger.info("Avalanche (Cme-hw) has STOPPED on exception {0}".format(e))
+		LOGGER.info("Avalanche (Cme-hw) has STOPPED on exception {0}".format(e))
 
 		# re-raise to print stack trace here (useful for debugging the problem)
 		raise
